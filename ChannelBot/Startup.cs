a86b9c2faf82cc91fe869d09371432dffc2d317c
OneZeroZeroOneOne <proxorenko200055@gmail.com>
2020-11-28 19:26:20 +0200

    datebaseconnectionstring

--- ChannelBot/Startup.cs
+++ ChannelBot/Startup.cs
@@ -37,10 +37,10 @@ namespace ChannelBot
             string datebaseconnectionstring = Environment.GetEnvironmentVariable("datebaseconnectionstring");
             services.AddTransient(x =>
             {
-                return new MainContext("Host=95.214.9.14;Database=postgres;Username=postgres;Password=123456rtyu");
+                return new MainContext(datebaseconnectionstring);
             });
 
-            MainContext context = new MainContext("Host=95.214.9.14;Database=postgres;Username=postgres;Password=123456rtyu");
+            MainContext context = new MainContext(datebaseconnectionstring);
 
             JwtOption jwtOption = context.JwtOption.FirstOrDefault();
 
